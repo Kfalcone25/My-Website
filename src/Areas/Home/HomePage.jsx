ccf6be61774c25e410885c46bdebbe86045c0dfb
Kflacone <kfalcone@dlr360.com>
2024-01-26 09:42:16 -0700

    First commit

---
+++ src/Areas/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Kristopher Falcone</h1>
+      <p>Im a freelance developer looking to get you the results you need.</p>
+      <button type="button">Contact Me</button>
+    </div>
+  );
+};
+
+export default HomePage;
